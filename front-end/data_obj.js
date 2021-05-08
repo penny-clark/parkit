b9ade53297d188cb9af22edda29f2844873df19f
NoahJung <noahjung87@gmail.com>
2021-05-08 11:23:03 -0700

    sample data obj

---
+++ front-end/data_obj.js
@@ -0,0 +1,156 @@
+const spots = [
+  {
+    id: 1,
+    owner: {
+      user_id: 1,
+      first_name: 'Eggert',
+      last_name:'Eggerson',
+      owner_email:'egg@egg.com',
+      avatar: 'https://pr.sssagent.com/img/a1.png'
+    },
+    street_address: '1081 Burrard St',
+    city: 'Vancouver',
+    province: 'BC',
+    country: 'Canada',
+    price: 16,
+    picture : 'https://pr.sssagent.com/img/parkingspot1.png',
+    rating: 4
+  },
+  {
+    id: 2,
+    owner: {
+      user_id: 4,
+      first_name: 'Frank',
+      last_name:'Faststuff',
+      owner_email:'franky@frank.com',
+      avatar: 'https://pr.sssagent.com/img/a4.png'
+    },
+    street_address: '1950 Franklin St',
+    city: 'Vancouver',
+    province: 'BC',
+    country: 'Canada',
+    price: 16,
+    picture : 'https://pr.sssagent.com/img/parkingspot2.png',
+    rating: 3
+  },
+  {
+    id: 3,
+    owner: {
+      user_id: 5,
+      first_name: 'Beth',
+      last_name:'Betilda',
+      owner_email:'bethy@gmail.com',
+      avatar: 'https://pr.sssagent.com/img/a5.png'
+    },
+    street_address: '401 W Georgia St',
+    city: 'Vancouver',
+    province: 'BC',
+    country: 'Canada',
+    price: 16,
+    picture : 'https://pr.sssagent.com/img/parkingspot3.png',
+    rating: 5
+  },
+  {
+    id: 4,
+    owner: {
+      user_id: 8,
+      first_name: 'Hilda',
+      last_name:'Sprockets',
+      owner_email:'hideehilda@gmail.com',
+      avatar: 'https://pr.sssagent.com/img/a8.png'
+    },
+    street_address: '385 Robson St',
+    city: 'Vancouver',
+    province: 'BC',
+    country: 'Canada',
+    price: 16,
+    picture : 'https://pr.sssagent.com/img/parkingspot4.png',
+    rating: 4
+  },
+  {
+    id: 5,
+    owner: {
+      user_id: 9,
+      first_name: 'TeeJay',
+      last_name:'Grammar',
+      owner_email:'tj@gmail.com',
+      avatar: 'https://pr.sssagent.com/img/a9.png'
+    },
+    street_address: '900 Burrard St',
+    city: 'Vancouver',
+    province: 'BC',
+    country: 'Canada',
+    price: 16,
+    picture : 'https://pr.sssagent.com/img/parkingspot5.png',
+    rating: 2
+  },
+  {
+    id: 6,
+    owner: {
+      user_id: 11,
+      first_name: 'Greg',
+      last_name:'Gregerson',
+      owner_email:'gregly@gmail.com',
+      avatar: 'https://pr.sssagent.com/img/a11.png'
+    },
+    street_address: '1025 Nelson St',
+    city: 'Vancouver',
+    province: 'BC',
+    country: 'Canada',
+    price: 16,
+    picture : 'https://pr.sssagent.com/img/parkingspot6.png',
+    rating: null
+  },
+  {
+    id: 7,
+    owner: {
+      user_id: 12,
+      first_name: 'Brad',
+      last_name:'Wong',
+      owner_email:'bwong@gmail.com',
+      avatar: 'https://pr.sssagent.com/img/a12.png'
+    },
+    street_address: '1133 Barclay St',
+    city: 'Vancouver',
+    province: 'BC',
+    country: 'Canada',
+    price: 16,
+    picture : 'https://pr.sssagent.com/img/parkingspot7.png',
+    rating: null
+  },
+  {
+    id: 8,
+    owner: {
+      user_id: 13,
+      first_name: 'Amy',
+      last_name:'Schindler',
+      owner_email:'aamyss@hey.com',
+      avatar: 'https://pr.sssagent.com/img/a13.png'
+    },
+    street_address: '1255 Bidwell St',
+    city: 'Vancouver',
+    province: 'BC',
+    country: 'Canada',
+    price: 16,
+    picture : 'https://pr.sssagent.com/img/parkingspot8.png',
+    rating: null
+  },
+  {
+    id: 9,
+    owner: {
+      user_id: 14,
+      first_name: 'Marie',
+      last_name:'Barker',
+      owner_email:'mariebarker22@gamil.com',
+      avatar: 'https://pr.sssagent.com/img/a14.png'
+    },
+    street_address: '1850 Comox St',
+    city: 'Vancouver',
+    province: 'BC',
+    country: 'Canada',
+    price: 5,
+    picture : 'https://pr.sssagent.com/img/parkingspot9.png',
+    rating: null
+  },
+]
+
