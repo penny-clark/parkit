c58362687bc25476cecfdb5880c5592e069f3a8d
penny-clark <respect.cactus@gmail.com>
2021-05-08 14:13:45 -0400

    Add fake data to cars table

---
+++ back-end/db/seeds/02_cars_seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO cars (
+  user_id,
+  make,
+  model,
+  colour,
+  plate_number
+)
+VALUES (1, 'Ford', 'Prius', 'Red', 'AAAAAA'),
+(6, 'Chevrolet', 'Malibu', 'Pink', 'CCCCCC'),
+(7, 'Hyudai', 'Elantra', 'Silver', 'TTTTTT'),
+(10, 'Toyota', 'Camry', 'Orange', 'OOOOOO'),
+(2, 'Volkwagon', 'Golf', 'Lavender', 'YUYUYU')
