0db3bd489e31c1a696e3a69975d6cafcebc3ddfe
penny-clark <respect.cactus@gmail.com>
2021-05-08 20:16:23 -0400

    Add fake test data to seed files

--- back-end/db/seeds/01_users_seeds.sql
+++ back-end/db/seeds/01_users_seeds.sql
@@ -4,18 +4,19 @@ INSERT INTO users (
   email,
   avatar
 )
-VALUES ('Eggert', 'Eggerson', 'egg@egg.com'),
-('Tilda', 'Tildy', 'tildy@gmail.com'),
-('Bob', 'Boberson', 'bob@bob.com'),
-('Frank', 'Faststuff', 'franky@frank.com'),
-('Beth', 'Betilda', 'bethy@gmail.com'),
-('Esther', 'Catchemall',  'essie@gmail.com'),
-('John', 'Stamos', 'jayjay@gmail.com'),
-('Hilda', 'Sprockets', 'hideehilda@gmail.com'),
-('TeeJay', 'Grammar', 'tj@gmail.com'),
-('Greg', 'Gregerson', 'gregly@gmail.com'),
-('Noah', 'Jin', 'noahjinn@gmail.com'),
-('Brad', 'Wong', 'bwong@gmail.com'),
-('Amy', 'Schindler', 'aamys@hey.com'),
-('Marie', 'Barker', 'mariebarker22@gamil.com')
+VALUES ('Eggert', 'Eggerson', 'egg@egg.com', 'https://pr.sssagent.com/img/a1.png'),
+('Tilda', 'Tildy', 'tildy@gmail.com', 'https://pr.sssagent.com/img/a2.png'),
+('Bob', 'Boberson', 'bob@bob.com', 'https://pr.sssagent.com/img/a3.png'),
+('Frank', 'Faststuff', 'franky@frank.com', 'https://pr.sssagent.com/img/a4.png'),
+('Beth', 'Betilda', 'bethy@gmail.com', 'https://pr.sssagent.com/img/a5.png'),
+('Esther', 'Catchemall',  'essie@gmail.com', 'https://pr.sssagent.com/img/a6.png'),
+('John', 'Stamos', 'jayjay@gmail.com', 'https://pr.sssagent.com/img/a7.png'),
+('Hilda', 'Sprockets', 'hideehilda@gmail.com', 'https://pr.sssagent.com/img/a8.png'),
+('TeeJay', 'Grammar', 'tj@gmail.com', 'https://pr.sssagent.com/img/a9.png'),
+('Greg', 'Gregerson', 'gregly@gmail.com', 'https://pr.sssagent.com/img/a10.png'),
+('Noah', 'Jin', 'noahjinn@gmail.com', 'https://pr.sssagent.com/img/a11.png'),
+('Brad', 'Wong', 'bwong@gmail.com', 'https://pr.sssagent.com/img/a12.png'),
+('Amy', 'Schindler', 'aamys@hey.com', 'https://pr.sssagent.com/img/a13.png'),
+('Marie', 'Barker', 'mariebarker22@gamil.com', 'https://pr.sssagent.com/img/a14.png'),
+('Brett', 'Bretterson', 'bb@bb.com')
 
--- back-end/db/seeds/03_spots_seeds.sql
+++ back-end/db/seeds/03_spots_seeds.sql
@@ -5,4 +5,14 @@ INSERT INTO spots (
   country,
   postal_code,
   picture,
+  price
 )
+VALUES (1, '1081 Burrard St', 'Vancouver', 'BC', 'Canada', 'V6Z 1Y6', 16),
+(4, '1950 Franklin St', 'Vancouver', 'BC', 'Canada', 'V5L 1R2', 3),
+(5, '401 W Georgia St', 'Vancouver', 'BC', 'Canada', 'V6B 5A1', 2),
+(8, '385 Robson St', 'Vancouver', 'BC', 'Canada', 'V6B 0H3', 2),
+(9, '900 Burrard St', 'Vancouver', 'BC', 'Canada', 'V6Z 3G5', 1),
+(11, '1025 Nelson St', 'Vancouver', 'BC', 'Canada', 'V6E 1J1', 3),
+(12, '1133 Barclay St', 'Vancouver', 'BC', 'Canada', 'V6E 1G8', 3),
+(13, '1255 Bidwell St', 'Vancouver', 'BC', 'Canada', 'V6G 2K8', 4),
+(14, '1850 Comox St', 'Vancouver', 'BC', 'Canada', 'V6G 2P4', 5) 
