0f620de8716f03cbdf3f94fb88e83f95a120cab8
NoahJung <noahjung87@gmail.com>
2021-05-10 09:55:18 -0700

    change .css to .scss

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import './App.css';
+import './App.scss';
 import Button from '@material-ui/core/Button';
 
 class App extends Component {
