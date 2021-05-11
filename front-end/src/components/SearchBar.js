6fdafc17fbf36d4178ea2e5fbee47d7da9fc0ca6
NoahJung <noahjung87@gmail.com>
2021-05-11 16:20:17 -0700

    install data-fns & picker

--- front-end/src/components/SearchBar.js
+++ front-end/src/components/SearchBar.js
@@ -1,8 +1,7 @@
 import React, { Component, useState } from 'react';
 
 import './SearchBar.scss';
-import { Button, Typography} from '@material-ui/core';
-import { OutlinedInput } from '@material-ui/core';
+import { FilledInput } from '@material-ui/core';
 import InputLabel from '@material-ui/core/InputLabel';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import FormControl from '@material-ui/core/FormControl';
@@ -10,14 +9,20 @@ import { Grid, TextField } from '@material-ui/core';
 import AddLocationRoundedIcon from '@material-ui/icons/AddLocationRounded';
 import AlarmRoundedIcon from '@material-ui/icons/AlarmRounded';
 import AlarmOnRoundedIcon from '@material-ui/icons/AlarmOnRounded';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
 
 export default function SearchBar(props) {
 
   return (
     <div class="searchbox">
-      <FormControl fullWidth variant="outlined">
+      <FormControl fullWidth variant="filled">
        <InputLabel htmlFor="outlined-adornment-amount">Postal code</InputLabel>
-          <OutlinedInput
+          <FilledInput
             id="outlined-adornment-amount"
             //value={values.amount}
             //onChange={handleChange('amount')}
@@ -26,9 +31,54 @@ export default function SearchBar(props) {
           />
         </FormControl>
         
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <Grid container justify="space-around">
+        
+        <KeyboardDatePicker
+          margin="normal"
+          id="date-picker-dialog"
+          label="Date picker dialog"
+          format="MM/dd/yyyy"
+          //value={selectedDate}
+         // onChange={handleDateChange}
+          KeyboardButtonProps={{
+            'aria-label': 'change date',
+          }}
+        />
+        <KeyboardTimePicker
+          margin="normal"
+          id="time-picker"
+          label="Time picker"
+         // value={selectedDate}
+         // onChange={handleDateChange}
+          KeyboardButtonProps={{
+            'aria-label': 'change time',
+          }}
+        />
+        <KeyboardTimePicker
+          margin="normal"
+          id="time-picker"
+          label="Time picker"
+         // value={selectedDate}
+         // onChange={handleDateChange}
+          KeyboardButtonProps={{
+            'aria-label': 'change time',
+          }}
+        />
+         <KeyboardDatePicker
+          margin="normal"
+          id="date-picker-dialog"
+          label="Date picker dialog"
+          format="MM/dd/yyyy"
+          //value={selectedDate}
+         // onChange={handleDateChange}
+          KeyboardButtonProps={{
+            'aria-label': 'change date',
+          }}
+        />
+      </Grid>
+    </MuiPickersUtilsProvider>
       
-      
-
      
 
 
