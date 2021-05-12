8c286a95ad9d1c005cef5ca8014bf85469f5b47c
NoahJung <noahjung87@gmail.com>
2021-05-12 08:00:45 -0700

    delete fetch data test

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -10,43 +10,12 @@ import SearchResult from './SearchResult';
 
 class App extends Component {
 
-  /* for fetch data 
-  constructor(props) {
-    super(props)
-    this.state = {
-      message: 'Hi! Click the button to load data!'
-    }
-  }
-
-  fetchData = () => {
-    axios.get('/api/spots') // You can simply make your requests to "/api/whatever you want"
-    .then((response) => {
-      // handle success
-      console.log(response.data) // The entire response from the Rails API
-      const onespot = response.data["1"].street_address
-      const printdata = JSON.stringify(onespot)
-      // console.log(response.data.message) // Just the message
-      this.setState({
-         message: printdata
-       });
-    }) 
-  }
-*/
+  
   render() {
     return (
 
       <div className="main">
-   
-          
-{/* 
-          <Typography variant="body1">
-            {this.state.message }
-          </Typography>
-            
-          <Button variant="contained" color="primary" onClick={this.fetchData}>
-            Fetch Data : street_address of id:1 
-          </Button>     */}
-          
+
           <TopBar />
           <SearchBar />
           <SearchResult />
