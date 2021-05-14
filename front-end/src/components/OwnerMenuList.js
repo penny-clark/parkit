e954829f6830a095c47c8ae8e707a1683d6717f8
NoahJung <noahjung87@gmail.com>
2021-05-14 04:02:08 -0700

    drawer close action fixed & icon added to menu

--- front-end/src/components/OwnerMenuList.js
+++ front-end/src/components/OwnerMenuList.js
@@ -2,32 +2,36 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link, useParams } from 'react-router-dom';
 import './TopBar.scss';
 import { List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
-
-
+import FolderRoundedIcon from '@material-ui/icons/FolderRounded';
+import EditLocationRoundedIcon from '@material-ui/icons/EditLocationRounded';
+import AddLocationRoundedIcon from '@material-ui/icons/AddLocationRounded';
 
 export default function OwnerMenuList(props) {
   return (
     <List>
       <Link to="/mybookedschedule" >
-        <ListItem button>
+        <ListItem button onClick={props.setDrawer}> 
           <ListItemIcon>
+            <FolderRoundedIcon className="icon_with_h6"/>
             <ListItemText>Booked Schedule</ListItemText>
           </ListItemIcon>
-        </ListItem>
+         </ListItem>
       </Link>
         <Link to="/myspots" >
-        <ListItem button>
+          <ListItem button onClick={props.setDrawer}>
           <ListItemIcon>
+            <EditLocationRoundedIcon className="icon_with_h6"/>
             <ListItemText>My Spots</ListItemText>
           </ListItemIcon>
-        </ListItem>
+         </ListItem>
         </Link>
       <Link to="/addnewspot" >
-        <ListItem button>
+        <ListItem button onClick={props.setDrawer}>
           <ListItemIcon>
+            <AddLocationRoundedIcon className="icon_with_h6"/>
             <ListItemText>Add New Spot</ListItemText>
           </ListItemIcon>
-        </ListItem>
+         </ListItem>
         </Link>
       </List>
   );
