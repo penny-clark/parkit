2df1874e069ca23527aef40f68438e17ddb3a10a
NoahJung <noahjung87@gmail.com>
2021-05-15 14:35:19 -0700

    added maker code & zoom setup true

--- front-end/src/components/MapView.jsx
+++ front-end/src/components/MapView.jsx
@@ -8,11 +8,17 @@ export default function MapView(props) {
     <div>
     <Typography variant="6" >This is map view</Typography>
     <div id="map" className="mapBox">
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+
+      <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
        
       </MapContainer>
     </div>
