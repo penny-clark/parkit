646f34681a5b94b5517fa10d74299604312e3748
NoahJung <noahjung87@gmail.com>
2021-05-15 18:45:01 -0700

    map popup styling

--- front-end/src/components/MapView.jsx
+++ front-end/src/components/MapView.jsx
@@ -1,12 +1,14 @@
 import React, {useState} from "react";
-import { Button, Typography, Grid, ListItem, List, Box} from '@material-ui/core';
+import { Button, Typography, Box} from '@material-ui/core';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import './Popup.scss';
+import SpotItem from './SpotItem';
+import BookingPopup from "./BookingPopup";
+import Paper from '@material-ui/core/Paper';
 
 export default function MapView(props) {
 
   return(
-    <div>
-    <Typography variant="6" >This is map view</Typography>
     <div id="map" className="mapBox">
       <MapContainer center={[49.28214439794972, -123.10596956451279]} zoom={14} scrollWheelZoom={true}>
         <TileLayer
@@ -17,10 +19,8 @@ export default function MapView(props) {
       {props.spots.map(spot => (
       <Marker key={spot.id} position={[spot.latitude, spot.longitude]}>
           <Popup position={[spot.latitude, spot.longitude]}>
-           <div>
-             <p>{spot.street_address}</p>
-             <p>Price: {spot.price}</p>
-             <button>Book now</button>
+           <div className="map_popup_wrap">
+            <SpotItem spot={spot} user={props.user} bookSpot={props.bookSpot} />
            </div>
           </Popup>
         </Marker>
@@ -28,7 +28,8 @@ export default function MapView(props) {
        
       </MapContainer>
     </div>
-    </div>
+
+ 
   )
 
 }
