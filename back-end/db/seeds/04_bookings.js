f763992bc466e505eee9c68ef46032ed29496a6f
penny-clark <respect.cactus@gmail.com>
2021-05-17 14:02:45 -0400

    Add more rating seeds and more varied bookings for user 1

--- back-end/db/seeds/04_bookings.js
+++ back-end/db/seeds/04_bookings.js
@@ -6,7 +6,7 @@ exports.seed = function(knex) {
       // Inserts seed entries
       return knex('bookings').insert([
         {car_id: 1, spot_id: 9, start_date_time: '2021-05-01 8:00:00', end_date_time: '2021-05-01 11:00:00'},
-        {car_id: 2, spot_id: 2, start_date_time: '2021-05-02 11:00:00', end_date_time: '2021-05-02 15:00:00'},
+        {car_id: 1, spot_id: 2, start_date_time: '2021-05-02 11:00:00', end_date_time: '2021-05-02 15:00:00'},
         {car_id: 5, spot_id: 5, start_date_time: '2021-05-03 12:00:00', end_date_time: '2021-05-03 20:00:00'},
         {car_id: 3, spot_id: 1, start_date_time: '2021-05-03 13:00:00', end_date_time: '2021-05-03 14:00:00'},
         {car_id: 1, spot_id: 9, start_date_time: '2021-05-03 8:00:00', end_date_time: '2021-05-03 11:00:00'},
@@ -15,10 +15,11 @@ exports.seed = function(knex) {
         {car_id: 5, spot_id: 4, start_date_time: '2021-05-24 16:00:00', end_date_time: '2021-05-24 18:00:00'},
         {car_id: 1, spot_id: 9, start_date_time: '2021-05-24 17:00:00', end_date_time: '2021-05-24 20:00:00'},
         {car_id: 2, spot_id: 9, start_date_time: '2021-06-01 13:00:00', end_date_time: '2021-06-01 14:00:00'},
-        {car_id: 2, spot_id: 8, start_date_time: '2021-06-01 14:00:00', end_date_time: '2021-06-01 17:00:00'},
+        {car_id: 1, spot_id: 8, start_date_time: '2021-06-01 14:00:00', end_date_time: '2021-06-01 17:00:00'},
         {car_id: 5, spot_id: 4, start_date_time: '2021-06-04 13:00:00', end_date_time: '2021-06-04 15:00:00'},
         {car_id: 5, spot_id: 1, start_date_time: '2021-06-05 13:00:00', end_date_time: '2021-06-05 14:00:00'},
-        {car_id: 4, spot_id: 1, start_date_time: '2021-06-06 13:00:00', end_date_time: '2021-06-06 16:00:00'}
+        {car_id: 4, spot_id: 1, start_date_time: '2021-06-06 13:00:00', end_date_time: '2021-06-06 16:00:00'},
+        {car_id: 1, spot_id: 4, start_date_time: '2021-06-07 13:00:00', end_date_time: '2021-06-07 16:00:00'}
         
       ]);
     });
