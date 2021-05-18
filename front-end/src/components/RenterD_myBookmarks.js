7b5d3a34853c2ef2eb10b0b49daa000e5352e25e
penny-clark <respect.cactus@gmail.com>
2021-05-18 12:49:37 -0400

    fix to bookmark expanded prop issue

--- front-end/src/components/RenterD_myBookmarks.js
+++ front-end/src/components/RenterD_myBookmarks.js
@@ -6,13 +6,13 @@ import SpotList from "./SpotList";
 import { getRenterBookmarks } from '../helpers/selector';
 
 export default function RenterD_myBookmarks(props) {
-
+  const [expanded, setExpanded] = useState(null);
   //get this renter's bookmark list from helper function
   const thisUserBookmarks = getRenterBookmarks(props.user.id, props.bookmarks)
-  console.log(thisUserBookmarks, "this user bookmarks")
+  
   return (
     <div className="wrap_dashboard">
-      <SpotList spots={thisUserBookmarks} user={props.user} bookSpot={props.bookSpot} />    
+      <SpotList spots={thisUserBookmarks} user={props.user} bookSpot={props.bookSpot} expanded={expanded} setExpanded={setExpanded} />    
     </div>
   );
 }
