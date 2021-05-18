c28028ea3e81de021c3efa8cb00a36801c9a2096
penny-clark <respect.cactus@gmail.com>
2021-05-17 23:16:47 -0400

    change to ratings seeds

--- back-end/db/seeds/07_spot_ratings.js
+++ back-end/db/seeds/07_spot_ratings.js
@@ -9,11 +9,11 @@ exports.seed = function(knex) {
         {user_id: 6, spot_id: 2,rating: 3},
         {user_id: 2, spot_id: 3,rating: 5},
         {user_id: 7, spot_id: 4,rating: 4},
-        {user_id: 1, spot_id: 5,rating: 2},
+        {user_id: 10, spot_id: 5,rating: 2},
         {user_id: 10, spot_id: 6,rating: 5},
         {user_id: 7, spot_id: 7,rating: 4},
-        {user_id: 1, spot_id: 8,rating: 1},
-        {user_id: 1, spot_id: 9,rating: 5}
+        {user_id: 10, spot_id: 8,rating: 1},
+        {user_id: 10, spot_id: 9,rating: 5}
       ]);
     });
 };
