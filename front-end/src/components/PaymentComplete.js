06420277dc63371d711c2aa5d0b68fa32852f975
NoahJung <noahjung87@gmail.com>
2021-05-18 20:01:31 -0700

    payment page styling

--- front-end/src/components/PaymentComplete.js
+++ front-end/src/components/PaymentComplete.js
@@ -10,10 +10,10 @@ export default function PaymentComplete(props) {
   return(
     <div className="paymentdone_wrap">
      <img src={Payment_gif}/>
-     <Typography variant="h6">Your payment is completed!</Typography>
-     <Typography variant="subtitle1">Go to check your booking list!</Typography>
+     <Typography variant="h4">Your payment is completed!</Typography>
+     <Typography variant="h6" className="page_title">Go to check your booking list!</Typography>
      <Link to="/mybookings" >
-     <Button variant="contained" className="gotoBookingBT">My Booking list</Button>
+     <Button variant="contained" className="gotoBookingBT" size="large" color="secondary">My Booking list</Button>
      </Link>
     </div>
   )
