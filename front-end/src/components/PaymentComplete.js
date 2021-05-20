96f1ededa04b5d148e73718868bcb92ea69976d1
NoahJung <noahjung87@gmail.com>
2021-05-19 17:10:47 -0700

    payment gif image link changed

--- front-end/src/components/PaymentComplete.js
+++ front-end/src/components/PaymentComplete.js
@@ -1,15 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './BookingProcess.scss';
-import Payment_gif from '../img/checkout.gif'
-import CheckCircleRoundedIcon from '@material-ui/icons/CheckCircleRounded';
 import { Button, Typography } from '@material-ui/core';
 
 export default function PaymentComplete(props) {
 
   return(
     <div className="paymentdone_wrap">
-     <img src={Payment_gif}/>
+     <img src="https://pr.sssagent.com/img/payment.gif"/>
      <Typography variant="h4">Your payment is completed!</Typography>
      <Typography variant="h6" className="page_title">Go to check your booking list!</Typography>
      <Link to="/mybookings" >
